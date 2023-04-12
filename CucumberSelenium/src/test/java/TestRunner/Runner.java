a0c7e91caa1eda62910877357cdd167abbf13e9c
ranji <ranji@DESKTOP-5RHTPCO>
2023-04-12 19:55:13 +0530

    rk

--- CucumberSelenium/src/test/java/TestRunner/Runner.java
+++ CucumberSelenium/src/test/java/TestRunner/Runner.java
@@ -6,7 +6,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 		features = {"src/test/java/features/login.feature"},
-		dryRun = !true,
+		dryRun = true,
 		glue="Steps",
 		snippets = SnippetType.CAMELCASE,
 		monochrome = true
